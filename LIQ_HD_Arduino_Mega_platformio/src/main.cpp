d4d6101ee5a772494d138a83664dfc2dac413b8f
David (secondary Dell) <davidswygart@gmail.com>
2026-01-22 12:16:34 -0500

    working SD write
    We don't know why we had to put everthing in the main function, but it didn't seem to even run the subfuctions otherwise.

--- LIQ_HD_Arduino_Mega_platformio/src/main.cpp
+++ LIQ_HD_Arduino_Mega_platformio/src/main.cpp
@@ -1,19 +1,55 @@
 
 #include <Arduino.h>
-#include "globals.h"
+
+#define chipSelect 4
 // #include "sensor_functions.h"
-#include "SD_functions.h"
+#include <SdFat.h>
 // #include "serial_functions.h"
 
 // Settings settings;
 
 void setup(){
 	Serial.begin(115200);
+	delay(6000);
 	Serial.println("System ready.");
 }
 
 void loop() {
-	bool create_log_file();
+	Serial.println("going_to_create_log_file");
+		Serial.println("Should be starting the create_log_file function");
+	
+		SdFat SD;
+	if (!SD.begin(chipSelect)) {
+		Serial.println("SD card initialization failed!");
+	}
+
+	char logFileName[40];
+	snprintf(logFileName, sizeof(logFileName),"licks.csv");
+
+	File dataFile;
+	dataFile = SD.open(logFileName, FILE_WRITE);
+	if (!dataFile) {
+		Serial.print("Error creating "); Serial.println(logFileName);
+	}
+
+	Serial.print("Writing headers to:"); Serial.println(logFileName);
+	dataFile.println("sipper_id,timestamp,state");
+
+
 	// void log_to_SD(signed char sipper_id, unsigned long timestamp, signed char state);
-	void close_log_file();
+		if (dataFile) {
+		dataFile.close();
+		Serial.println("Log file closed.");
+	} else {
+		Serial.println("No log file to close.");
+	}
+
+	Serial.println("going_to_cloe_log_file");
+
+	int c = 0;
+	while (true){
+		Serial.println(c);
+		delay(1000);
+		c++;
+	}
 }
